c36759b9826b14e5b5f99c63d5c60e5158bd3f4b
ZuoYaoyao <ultimatezy@foxmail.com>
2017-11-22 19:19:18 +0800

    11-22

--- ch1.py
+++ ch1.py
@@ -1,2 +1,9 @@
-def  dd:
-    a=12
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+s = pd.Series([1,3,5,np.nan,6,8])
+print(s)
+
+dates=pd.date_range('20170203',periods=9)
+print(dates)
